69b12fd05af299f2ec85c84833e5dcf56c48a751
kris1027 <krzys89@gmail.com>
2026-01-16 23:53:02 +0100

    feat: add asyncHandler wrapper to all controllers for safe async error handling

--- src/controllers/addresses.ts
+++ src/controllers/addresses.ts
@@ -1,35 +1,36 @@
 import type { Request, Response } from 'express';
 import { addressService } from '../services/addresses.js';
 import { sendSuccess, sendCreated } from '../utils/success.js';
+import { asyncHandler } from '../utils/async-handler.js';
 
 export const addressController = {
-  getAll: (req: Request, res: Response) => {
+  getAll: asyncHandler((req: Request, res: Response) => {
     const addresses = addressService.getAllByUser(req.user!.userId);
     sendSuccess(res, addresses);
-  },
+  }),
 
-  getById: (req: Request, res: Response) => {
+  getById: asyncHandler((req: Request, res: Response) => {
     const address = addressService.getById(req.params.id as string, req.user!.userId);
     sendSuccess(res, address);
-  },
+  }),
 
-  create: (req: Request, res: Response) => {
+  create: asyncHandler((req: Request, res: Response) => {
     const address = addressService.create(req.user!.userId, req.body);
     sendCreated(res, address);
-  },
+  }),
 
-  update: (req: Request, res: Response) => {
+  update: asyncHandler((req: Request, res: Response) => {
     const address = addressService.update(req.params.id as string, req.user!.userId, req.body);
     sendSuccess(res, address);
-  },
+  }),
 
-  delete: (req: Request, res: Response) => {
+  delete: asyncHandler((req: Request, res: Response) => {
     const address = addressService.delete(req.params.id as string, req.user!.userId);
     sendSuccess(res, address);
-  },
+  }),
 
-  setDefault: (req: Request, res: Response) => {
+  setDefault: asyncHandler((req: Request, res: Response) => {
     const address = addressService.setDefault(req.params.id as string, req.user!.userId);
     sendSuccess(res, address);
-  },
+  }),
 };
--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -4,42 +4,43 @@ import type { ProductQuery } from '../schemas/product.js';
 import { sendCreated, sendPaginated, sendSuccess } from '../utils/success.js';
 import { productService } from '../services/products.js';
 import { NotFoundError } from '../utils/errors.js';
+import { asyncHandler } from '../utils/async-handler.js';
 
 export const productController = {
   // GET /products
-  getAll: (_req: Request, res: Response) => {
+  getAll: asyncHandler((_req: Request, res: Response) => {
     const { page, limit, category, isFeatured } = res.locals.query as ProductQuery;
     const result = productService.getAll({ page, limit, category, isFeatured });
     sendPaginated(res, result.data, result.meta);
-  },
+  }),
 
   // GET /products/:identifier
-  getOne: (req: Request, res: Response) => {
+  getOne: asyncHandler((req: Request, res: Response) => {
     const identifier = req.params.identifier as string;
     const product = productService.getByIdentifier(identifier);
     if (!product) throw new NotFoundError('Product');
     sendSuccess(res, product);
-  },
+  }),
 
   // POST /products
-  create: (req: Request, res: Response) => {
+  create: asyncHandler((req: Request, res: Response) => {
     const product = productService.create(req.body);
     sendCreated(res, product);
-  },
+  }),
 
   // PUT /products/:id
-  update: (req: Request, res: Response) => {
+  update: asyncHandler((req: Request, res: Response) => {
     const id = req.params.id as string;
     const product = productService.update(id, req.body);
     if (!product) throw new NotFoundError('Product');
     sendSuccess(res, product);
-  },
+  }),
 
   // DELETE /products/:id
-  remove: (req: Request, res: Response) => {
+  remove: asyncHandler((req: Request, res: Response) => {
     const id = req.params.id as string;
     const product = productService.remove(id);
     if (!product) throw new NotFoundError('Product');
     sendSuccess(res, product);
-  },
+  }),
 };
